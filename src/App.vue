e142e2fcc1424eec6ef9021f05d43fd2c825b3f4
Jordan Caumartin <pro.jordanc@gmail.com>
2019-12-18 13:34:07 +0100

    create map in Nantes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Map
   }
 }
 </script>
