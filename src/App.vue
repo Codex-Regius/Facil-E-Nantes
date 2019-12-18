f65ec738c0013dc25f79fe168f750f5f2ec6569f
Martin Klauer <mdklauer@gmail.com>
2019-12-18 11:44:45 +0100

    ajout du header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header facil="Facil" enantes="E-Nantes"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <h1 class="facil">{{ facil }}</h1>
+    <h1>{{ enantes }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    facil: String,
+    enantes: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  background: lightgray;
+  margin: 0px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  padding: 20px
+}
+
+h1{
+  margin: 0px;
+  height: 100%;
+  color: #F18C00;
+}
+
+.facil{
+  color: #007BC1;
+  margin-right: 5px;
+}
+
+</style>
